38abc3c2aed2f85a78664b569d92213454dff1c3
hashkitti001 <iJeddy5.0@gmail.com>
2025-04-30 10:52:43 +0100

    adjusted design for swipe card component and fixed sound issue on swipe

---
+++ src/app/discover/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import Discover from "@/components/discover/Discover";
+
+export default function DiscoverPage() {
+    return (
+        <div className="flex mt-10 align-center">
+        <Discover />
+        </div>
+    )
+
+}
